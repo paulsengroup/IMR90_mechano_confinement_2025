b1247d1f27242005e19553ba5e892fbc304e4f93
odasho <39092095+odasho@users.noreply.github.com>
2025-10-16 13:27:48 +0200

    Add differential expression code

--- Downstream_analysis/diff_exp_analysis.R
+++ Downstream_analysis/diff_exp_analysis.R
@@ -1,15 +1,11 @@
-#Author: Oda Hovet
-
-
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-library(DESeq2)
 library('org.Hs.eg.db')
+library(DESeq2)
 library(writexl)
 library(dplyr)
 
-
 # Load counts table
-counts <- read.table(file = "salmon_gene_counts/salmon.merged.gene_counts.tsv", sep = '\t', header = TRUE)
+counts <- read.table(file = "path/raw_gene_counts_file", sep = '\t', header = TRUE)
 
 # Filter genes using hg38 annotation
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
@@ -25,7 +21,7 @@ gene_symbols <- AnnotationDbi::select(org.Hs.eg.db,
 
 # Keep only genes present in hg38
 countData <- counts %>%
-  filter(gene_name %in% gene_symbols$SYMBOL) %>%
+  dplyr::filter(gene_name %in% gene_symbols$SYMBOL) %>% 
   select(-gene_id) %>%
   group_by(gene_name) %>%
   summarise(across(everything(), sum)) %>%
@@ -35,11 +31,11 @@ countData <- counts %>%
 # Set gene names as row names
 rownames(countData) <- countData$gene_name
 countData$gene_name <- NULL
-countData <- countData[, !(colnames(countData) %in% c("gene_id", "15m_REP2"))]
+countData <- countData[, !(colnames(countData) %in% c("gene_id", "X15m_REP2"))]
 
 
 # Define batch variable
-batch_ex_deseq <- c(2, 2, 2, 2, 2, 1, 
+batch_ex_deseq <- c(2, 2, 2, 2, 1, 
                     2, 2, 2, 2, 2, 2, 
                     1, 1, 1, 1, 1, 1, 
                     1, 1, 1, 1, 1, 1, 
@@ -49,7 +45,7 @@ batch_ex_deseq <- factor(batch_ex_deseq)
 # Column metadata 
 col_data_ex_deseq <- data.frame(
   Sample = colnames(countData),
-  Condition = c(rep("15m", 6),
+  Condition = c(rep("X15m", 5),
                 rep("X1h", 6),
                 rep("X4h", 6),
                 rep("NC", 7),
@@ -79,6 +75,11 @@ run_deseq_comparison <- function(count_data, col_data,
   dds <- DESeq(dds)
   res <- results(dds)
   
+  res_ex_deseq <- data.frame(res)
+  
+  out_file_res <- paste0("corrected_res_", groupA, "_vs_", groupB, "_ex_deseq_ALL.xlsx")
+  write.xlsx(res_ex_deseq, out_file_res, rowNames = TRUE)
+  
   # Filter significant results
   res_filtered <- res[!is.na(res$padj) & res$padj < 0.05, ]
   res_filtered <- as.data.frame(res_filtered)
@@ -90,13 +91,16 @@ run_deseq_comparison <- function(count_data, col_data,
   return(res_filtered)
 }
 
+
+
 comparisons <- list(
-  c("NC", "15m"),
+  c("NC", "X15m"),
   c("NC", "X1h"),
   c("NC", "X4h"),
   c("NC", "ON")
 )
 
+# Filtered list
 results_list <- lapply(comparisons, function(x) {
   run_deseq_comparison(countData, col_data_ex_deseq, x[1], x[2], "ex_deseq")
 })
@@ -105,7 +109,7 @@ names(results_list) <- sapply(comparisons, function(x) paste(x, collapse = "_vs_
 
 
 
-results_list$NC_vs_15m
+results_list$NC_vs_X15m
 results_list$NC_vs_X1h
 results_list$NC_vs_X4h
 results_list$NC_vs_ON
